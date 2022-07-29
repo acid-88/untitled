ec0ce53fb78d0e9015914f60b322f906b1a4af52
acid-88 <acid.nation.work@gmail.com>
2022-07-29 17:54:38 +0300

    age (switch)
    возраст через switch и case

--- src/char2.java
+++ src/char2.java
@@ -1,14 +1,21 @@
 import java.util.Scanner;
 public class char2 {
     public static void main(String[] args) {
-        int i = 0;
-        while(true){
-            if(i==145){
+        Scanner x = new Scanner(System.in);
+        System.out.println("Vvedi age");
+        int age = x.nextInt();
+        switch(age){
+            case 0 :
+                System.out.println("Ti mladenec");
                 break;
-            }
-        i++;
-        System.out.println("Stakan " + i);
+            case 6 :
+                System.out.println("Ti poshel v shkolu");
+                break;
+            case 18 :
+                System.out.println("Ti poshel v univer");
+                break;
+            default:
+                System.out.println("Obichniy vozrast");
         }
-        System.out.println("Poigrali i hvatit!");
     }
 }
