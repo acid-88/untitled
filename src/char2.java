ab2eeb8dc40c05d7e038e6f21857eb5001bd1145
acid-88 <acid.nation.work@gmail.com>
2022-07-29 17:07:45 +0300

    new
    Анализ ввода пользователя игра угадай пятерку

--- src/char2.java
+++ src/char2.java
@@ -2,8 +2,17 @@ import java.util.Scanner;
 public class char2 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Chto to napishi");
-        String string = s.nextLine();
-        System.out.println("Sam ti " + string);
+        System.out.println("Ya zagadal chislo, poprobuy ugaday");
+        int x = s.nextInt();
+        while(x!=5){
+            System.out.println("HET, poprobuy ewe: ");
+            if (x<5) {
+                System.out.println("Bolshe!");
+            } else {
+                System.out.println("Menshe!");
+            }
+            x = s.nextInt();
+        }
+        System.out.println("Molodec!");
     }
 }
