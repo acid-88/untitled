6ef492e1e6ae056eebb641a1d88af66bda009a8f
acid-88 <acid.nation.work@gmail.com>
2022-07-29 14:22:32 +0300

    new string added

--- src/lesson_For.java
+++ src/lesson_For.java
@@ -1,8 +1,11 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class lesson_For {
     public static void main(String[] args) {
         for(int x=1;x <= 10; x++) {
             System.out.println("Vipivau " + x + " stakan");
         }
         System.out.println("Nu vse, ya nazhralsa");
+        System.out.println("Hello guys");
     }
 }
