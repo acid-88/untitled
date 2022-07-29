e76ae9b5991bee5012c05b2f1cb411aa914ae141
acid-88 <acid.nation.work@gmail.com>
2022-07-29 18:38:24 +0300

    massivi
    инициализация массива, два вида создания массива

--- src/char2.java
+++ src/char2.java
@@ -1,21 +1,16 @@
 import java.util.Scanner;
 public class char2 {
     public static void main(String[] args) {
-        Scanner x = new Scanner(System.in);
-        System.out.println("Vvedi age");
-        String age = x.nextLine();
-        switch(age){
-            case "DA" :
-                System.out.println("Ti mladenec");
-                break;
-            case "NET" :
-                System.out.println("Ti poshel v shkolu");
-                break;
-            case "HZ" :
-                System.out.println("Ti poshel v univer");
-                break;
-            default:
-                System.out.println("Obichniy vozrast");
+        int x = 10;           // примитивный тип данных           [10]
+        char g = 'a';           // примитивный тип данных
+        String k = "Hi";          // ссылочный тип данных
+        int[] mass = {2,4,5,6,7,8,34,234,6432};
+        int[] m1 = new int[5];      // ссылочный тип данных  [0 1 2 3 4]      // m1 -> [массив]
+        for (int i = 0; i < m1.length; i++) {    // инициализация массива с помощью цикла for
+            m1[i] = i * 10;
+        }
+        for (int i = 0; i < mass.length; i++) {
+            System.out.println("Vashe chislo iz massiva pod indeksom " + i + ":" + mass[i]);
         }
     }
 }
