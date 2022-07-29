50a995d2874c26b1e70c5a93253b8eb86c386d7a
acid-88 <acid.nation.work@gmail.com>
2022-07-29 15:40:01 +0300

    skanner zarabotal

---
+++ src/char2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class char2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Chto to napishi");
+        String string = s.nextLine();
+        System.out.println("TI napisal " + string);
+    }
+}
