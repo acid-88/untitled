08dba7eae4a1c3b965558b5fba5cd2179037fbe8
acid-88 <110033870+acid-88@users.noreply.github.com>
2022-07-30 11:15:00 +0300

    izmenenie

--- src/Eachfor.java
+++ src/Eachfor.java
@@ -1,10 +1,14 @@
 public class Eachfor {
     public static void main(String[] args) {
         int[] H = new int[5];   // массив целых чисел
-        H[0] = 4;        //инициализация первого индекса массива
-        H[1] = 8;
-        System.out.println(H[0]);    // выводим на консоль 1 и 3 индексы
-        System.out.println(H[2]);
+        H[0] = 7;
+        H[3] = 3;
+        int sum=0;
+        for(int x:H) {
+        sum = sum + x;
+        }
+        System.out.println(sum);
+        System.out.println();
         String[] stroke = new String[4];
         stroke[0] = "Hello";
         stroke[1] = " Java";
